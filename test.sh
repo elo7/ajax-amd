3b04264e2fc9de4fd312ddb7a0d4fc657d5bb52b
Fernanda Moraes Bernardo and Luiz Corte Real <login+FernandaBernardo+luiz@elo7.com>
2016-01-11 13:12:30 -0200

    Refactoring tests

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 ./node_modules/mocha-phantomjs/bin/mocha-phantomjs -R spec "http://localhost:8888/test/ajaxTest.js.html";
+./node_modules/mocha-phantomjs/bin/mocha-phantomjs -R spec "http://localhost:8888/test/ajaxGetTest.js.html";
+./node_modules/mocha-phantomjs/bin/mocha-phantomjs -R spec "http://localhost:8888/test/ajaxPostTest.js.html";
+./node_modules/mocha-phantomjs/bin/mocha-phantomjs -R spec "http://localhost:8888/test/ajaxSerializeTest.js.html";
